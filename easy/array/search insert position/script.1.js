5fd160987d17c3b7f60aef752eb729a47db94236
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-19 22:47:52 +0200

    more suitablу solution

--- easy/array/search insert position/script.1.js
+++ easy/array/search insert position/script.1.js
@@ -8,21 +8,21 @@ var nums = [1, 3, 5, 6];
 
 var searchInsert = function (nums, target) {
 
-    //     var index = nums.indexOf(target);
-    //     if (index >= 0) {
-    //         return index;
-    //     }
+    var index = nums.indexOf(target);
+    if (index >= 0) {
+        return index;
+    }
 
     for (var i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
-        }
+        // if (nums[i] === target) {
+        //     return i;
+        // }
         if (nums[i] > target) {
             return i;
         }
-        if (target === 0) {
-            return 0;
-        }
+        // if (target === 0) {
+        //     return 0;
+        // }
     }
     return nums.length;
 };
