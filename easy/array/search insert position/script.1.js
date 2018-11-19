d23327898487ebde8134de493de489a43fbc2b92
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-19 22:34:16 +0200

    add new ability

--- easy/array/search insert position/script.1.js
+++ easy/array/search insert position/script.1.js
@@ -1,9 +1,17 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 
 var nums = [1, 3, 5, 6];
 
 var searchInsert = function (nums, target) {
 
+    //     var index = nums.indexOf(target);
+    //     if (index >= 0) {
+    //         return index;
+    //     }
 
     for (var i = 0; i < nums.length; i++) {
         if (nums[i] === target) {
