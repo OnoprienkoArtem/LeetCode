fa0f269b05f6db9f159654adf51683406a8b6b5a
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-08 22:09:27 +0200

    solution from leetcode

---
+++ easy/array/Move Zeroes/script.1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nums = [0, 1, 0, 3, 12];
+
+var moveZeroes = function (nums) {
+    for (var i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] == 0) {
+            nums.splice(i, 1)
+            nums.push(0);
+        }
+    }
+};
+
+console.log(nums);
