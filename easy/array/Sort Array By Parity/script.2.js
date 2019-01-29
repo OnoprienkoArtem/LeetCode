e9d4bb2d53d15633d214394e0410f3bc6285ea8e
OnoprienkoArtem <o.artem.e@gmail.com>
2019-01-29 23:04:23 +0200

    add new solution from leetcode

---
+++ easy/array/Sort Array By Parity/script.2.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var nums = [3, 1, 2, 4];
+
+var sortArrayByParity = function (nums) {
+    var even = [];
+    var odd = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            even.push(nums[i]);
+        } else {
+            odd.push(nums[i]);
+        }
+    }
+    return even.concat(odd);
+};
+
+console.log(sortArrayByParity(nums));
